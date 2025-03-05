350dd0d283d6dbe116eb91043e1a1fe3a6ac5257
github-mariyaweb <96mariyask@gmail.com>
2025-03-05 22:17:20 +0300

    refactor: remove coments

--- src/shared/api/products/getFiltredGoods.js
+++ src/shared/api/products/getFiltredGoods.js
@@ -1,10 +1,5 @@
 import { apiRoot } from 'shared/api/commercetoolsClient';
 
-// 'variants.attributes.brand.key:"callaway"'
-// categories.id:"5b321035-7479-4d60-ace1-dfbb47604f7e"
-// filter: ['categories.id:"5b321035-7479-4d60-ace1-dfbb47604f7e"', 'variants.attributes.brand.key:"callaway"'],
-// variants.attributes.New:"true"
-// const testdata = ['variants.attributes.brand.key:"stromberg", "callaway"'];
 export const getFiltredGoods = async (limitValue = 15, offsetValue = 0, filterArr = []) => {
   try {
     const response = await apiRoot
@@ -16,7 +11,6 @@ export const getFiltredGoods = async (limitValue = 15, offsetValue = 0, filterAr
           filter: [...filterArr],
         },
       }).execute();
-    console.log(response);
     if (!response || !response.body || !response.body.results) {
       throw new Error('API вернул некорректный ответ');
     }
